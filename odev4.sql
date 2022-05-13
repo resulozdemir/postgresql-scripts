84dffada1c3ade8547daa241ffcbb2aed9cb2777
Resul Ozdemir <zemirsd9@gmail.com>
2022-05-14 01:18:41 +0300

    dorduncu odev

---
+++ odev4.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT replacement_cost FROM film ;
+SELECT COUNT(DISTINCT replacement_cost) FROM film ;
+SELECT COUNT(*) FROM film where title LIKE 'T%' AND rating = 'G';
+SELECT COUNT(*) from country WHERE country like '_____';
+SELECT COUNT(*) FROM city WHERE city ILIKE '%R'; 
