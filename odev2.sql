abb681c4775cf1fd51520716774ac142607b4f2c
Resul Ozdemir <zemirsd9@gmail.com>
2022-05-13 16:33:28 +0300

    Revert "Create odev2.sql"
    This reverts commit 10042848501baf5dce6a1f97d16a69d48bfe11e7.

--- odev2.sql
+++
@@ -1,3 +0,0 @@
-select * from film where replacement_cost >= 12.99 AND replacement_cost < 16.99;
-select first_name,last_name from actor where first_name IN ('Penelope','Nick','Ed'); 
-select * from film where rental_rate IN (0.99,2.99,4.99) AND replacement_cost IN (12.99,15.99,28.99);
