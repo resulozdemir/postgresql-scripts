35781e9e3350b64ee3c53e675ba371c1aecbb93e
Resul Ozdemir <zemirsd9@gmail.com>
2022-05-14 00:42:02 +0300

    Revert "3.Ödev"
    This reverts commit ffee728dc608e9d65523ed94ad35da2753344355.

--- odev3.sql
+++
@@ -1,4 +0,0 @@
-select * from country where country LIKE 'A%a';
-select * from country where country LIKE '_____%n';
-select * from film  where title ILIKE '%T%T%T%T%';
-select * from film  where title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
